c2b135fef8cab5ab162abde2e2b691d8bdaa6767
Paulina-1983 <paulina1983@live.com.ar>
2021-07-27 20:58:28 -0300

    comenzando carrito de compras

--- sprint2/app.js
+++ sprint2/app.js
@@ -8,9 +8,11 @@ app.use(express.static('public'));
 
 app.get('/',(req,res) => res.sendFile(path.join(__dirname,'views','index.html')));
 app.get('/login',(req,res) => res.sendFile(path.join(__dirname,'views','login.html')));
-app.get('/cart',(req,res) => res.sendFile(path.join(__dirname,'views','productCart.html')));
-app.get('/detail',(req,res) => res.sendFile(path.join(__dirname,'views','productDetail.html')));
+app.get('/cart',(req,res) => res.sendFile(path.join(__dirname,'views','cart.html')));
+app.get('/detail',(req,res) => res.sendFile(path.join(__dirname,'views','Detail.html')));
 app.get('/register',(req,res) => res.sendFile(path.join(__dirname,'views','register.html')));
+app.get('/header',(req,res) => res.sendFile(path.join(__dirname,'views','header.html')));
+
 
 
 
