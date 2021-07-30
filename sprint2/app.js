0871214425802080959689d8e543bb788961b5f3
Patricia-27 <mari27velaz@gmail.com>
2021-07-30 20:14:15 -0300

    ....

--- sprint2/app.js
+++ sprint2/app.js
@@ -9,7 +9,7 @@ app.use(express.static('public'));
 app.get('/',(req,res) => res.sendFile(path.join(__dirname,'views','index.html')));
 app.get('/login',(req,res) => res.sendFile(path.join(__dirname,'views','login.html')));
 app.get('/cart',(req,res) => res.sendFile(path.join(__dirname,'views','productCart.html')));
-app.get('/detail',(req,res) => res.sendFile(path.join(__dirname,'views','Detail.html')));
+app.get('/detail',(req,res) => res.sendFile(path.join(__dirname,'views','detail.html')));
 app.get('/register',(req,res) => res.sendFile(path.join(__dirname,'views','register.html')));
 
 
