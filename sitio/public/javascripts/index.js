09f73b59e71022a18c3eeecb24e4e71c9a152926
pablitoxic <lobaipablo@gmail.com>
2021-09-03 11:36:41 -0300

    productos dinamicos

---
+++ sitio/public/javascripts/index.js
@@ -0,0 +1,54 @@
+new Glider(document.querySelector('.glider'), {
+    // Mobile-first defaults
+    itemWidth: 282,
+    slidesToShow: 'auto',
+    slidesToScroll: 1,
+    duration: 1.5,
+    scrollLock: true,
+    draggable: true,
+    dots: '.dots',
+    arrows: {
+      prev: '.glider-prev',
+      next: '.glider-next'
+    },
+    responsive: [
+      {
+        // screens greater than >= 580px
+        breakpoint: 580,
+        settings: {
+          // Set to `auto` and provide item width to adjust to viewport
+          slidesToShow: 'auto',
+          slidesToScroll: 1,
+          itemWidth: 282,
+          duration: 1.5
+        }
+      },{
+        // screens greater than >= 768px
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          itemWidth: 282,
+          duration: 1.5
+        }
+      },{
+        // screens greater than >= 1024px
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          itemWidth: 282,
+          duration: 1.5
+        }
+      },{
+        // screens greater than >= 1200px
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 2,
+          itemWidth: 282,
+          duration: 1.5
+        }
+      }
+    ]
+  });
--- sitio/src/controllers/indexController.js
+++ sitio/src/controllers/indexController.js
@@ -1,7 +1,13 @@
+const fs = require('fs');
+const path = require('path');
+const categorias = require('../data/categorias.json')
+const productos = JSON.parse(fs.readFileSync(path.join(__dirname, '..', 'data','productos.json'),'utf-8'));
+
 module.exports = {
     home : (req,res) => {
         return res.render('index',{
-            title : "Dulce Deleite"
+           productos,
+           categorias
         })
     },
     
