cec501aa199c2b7bf08f3688f3932caac9fea50e
pablitoxic <lobaipablo@gmail.com>
2021-09-06 12:48:26 -0300

    vistas de categorias

--- sitio/src/controllers/productsController.js
+++ sitio/src/controllers/productsController.js
@@ -1,7 +1,56 @@
+const fs = require('fs');
+const path = require('path')
+
+
+const categorias = require('../data/categorias.json');
+const productos = JSON.parse(fs.readFileSync(path.join(__dirname, '..', 'data','productos.json'),'utf-8'));
+
 module.exports = {
     detail: (req, res) => {
      return res.render('detail')},
+
     cart: (req, res) => {
         return res.render('cart')
+    },
+
+    categoryViewPos: (req,res) => {
+
+            return res.render('productsCatPostres', {
+                 productos,
+                 categorias
+                 
+             })
+  
+    },
+
+    categoryViewMuf: (req,res) => {
+
+        return res.render('productsCatMuffins', {
+            productos,
+            categorias
+            
+        })
+
+     },
+
+     categoryViewTor: (req,res) => {
+
+        return res.render('productsCatTortas', {
+            productos,
+            categorias
+            
+        })
+
+    },
+
+    categoryViewSal: (req,res) => {
+
+        return res.render('productsCatSaludable', {
+            productos,
+            categorias
+            
+        })
+
     }
+
 }
--- sitio/src/routes/products.js
+++ sitio/src/routes/products.js
@@ -1,10 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-const {detail,cart} = require('../controllers/productsController');
+const {detail,cart, categoryViewPos, categoryViewMuf, categoryViewTor, categoryViewSal} = require('../controllers/productsController');
 
 router.get('/cart',cart);
 router.get('/detail',detail);
+router.get('/categoriesPos', categoryViewPos);
+router.get('/categoriesMuf', categoryViewMuf);
+router.get('/categoriesTor', categoryViewTor);
+router.get('/categoriesSal', categoryViewSal);
 
 
 module.exports = router;
