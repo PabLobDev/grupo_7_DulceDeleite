f8bad7c37757ec3dc2aa953cfad287df1de0ccdb
Paulina-1983 <paulina1983@live.com.ar>
2021-10-10 23:52:41 -0300

    subo buscador

--- sitio/src/controllers/indexController.js
+++ sitio/src/controllers/indexController.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const categorias = require('../data/categorias.json')
 const productos = JSON.parse(fs.readFileSync(path.join(__dirname, '..', 'data','productos.json'),'utf-8'));
-
+const {Op} = require('sequelize');
 const db = require('../database/models');
 
 module.exports = {
@@ -19,17 +19,33 @@ module.exports = {
             })
             .catch(error => console.log(error))
     },
-    
     search : (req,res) => {
-        if(req.query.busqueda){
-            let resultado = productos.filter(producto => producto.nombre.toLowerCase().includes(req.query.busqueda.toLowerCase()))
-            return res.render('products/searchResults',{
-                title : "Resultado de la búsqueda",
-                productos : resultado,
-                busqueda : req.query.busqueda.trim()
+        db.Product.findAll({
+            include : ['category'],
+            where : {
+                [Op.or] : [
+                    {
+                        name : {
+                            [Op.substring] : req.query.busqueda
+                        }
+                    },
+                    {
+                        description : {
+                            [Op.substring] : req.query.busqueda
+                        }
+                    }
+                ]
+            }
+        })
+            .then(products => {
+                return res.render('./products/searchResults',{
+                    title : "Resultado de la búsqueda",
+                    products,
+                    busqueda : req.query.busqueda
+                })
             })
-        }
-        return res.redirect('/')
-    }
+            .catch(error => console.log(error)) 
+   
+},
     
 }
