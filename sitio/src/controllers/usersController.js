b51660b79487e6f082ac5c5d9f72e9ca412e08da
JoaquinLeguizamon <joaquinleguizamon03@gmail.com>
2021-10-19 14:22:41 -0300

    agrega crud users y script datos

--- sitio/src/controllers/usersController.js
+++ sitio/src/controllers/usersController.js
@@ -1,8 +1,6 @@
 const {validationResult} = require('express-validator');
 const bcrypt = require('bcryptjs');
-const users = require('../data/users.json');
-const fs = require('fs');
-const path = require('path');
+const db = require('../database/models');
 
 module.exports = {
     register: (req, res) => {
@@ -16,33 +14,31 @@ module.exports = {
         
         if(errors.isEmpty()){
             
-            const {name, last_name, age, city, email, pass} = req.body;
+            const {name, surname, age, city, email, pass} = req.body;
 
-            let user = {
-                id : users.length > 0 ? users[users.length - 1].id + 1 : 1,
+            db.User.create ({
                 name : name.trim(),
-                last_name : last_name.trim(),
+                surname : surname.trim(),
                 age : +age,
                 city: city.trim(),
                 email : email.trim(),
                 pass : bcrypt.hashSync(pass.trim(),10),
-                rol : 'user',
+                rolId : 1,
                 avatar : 'avatar_default.png'
-            }
-            users.push(user);
-            fs.writeFileSync(path.join(__dirname, '..','data','users.json'),JSON.stringify(users,null,2),'utf-8');
-
+            })
+               .then(user => {
             req.session.userLogin = {
                 id : user.id,
                 name : user.name,
                 avatar : user.avatar,
-                rol : user.rol
+                rolId : user.rolId
             }
             return res.redirect('/users/login')
+        })
+        .catch(error => console.log(error))
         }else{
             return res.render('users/register',{
-                title : "Intentalo de nuevo",
-                old : req.body,
+                title : "Algo no anduvo bien",
                 errors : errors.mapped()
             })
         }
@@ -57,19 +53,27 @@ module.exports = {
                 let errors = validationResult(req);
         
         if(errors.isEmpty()){
-            let user = users.find(user => user.email === req.body.email.trim())
+            
+            const {email,recordar} = req.body;
+
+             db.User.findOne({
+                 where : {
+                     email
+                 }
+             })
+             .then(user => {
 
             req.session.userLogin = {
                 id : user.id,
                 name : user.name,
                 avatar : user.avatar,
-                rol : user.rol
+                rolId : user.rolId
             }
-
-            if(req.body.recordar){
+             if(req.body.recordar){
                 res.cookie('ddLogin', req.session.userLogin, {maxAge : 2000 * 60 })
             }
             res.redirect('../')
+        })
         }else{
             return res.render('users/login',{
             title : "Intentalo de nuevo",
