ef7615bd6fc121313a32c7b6d6600bccb780067d
Paulina-1983 <paulina1983@live.com.ar>
2021-11-20 18:46:43 -0300

    agrego sweetalert2 en eliminacion

--- sitio/public/javascripts/profileUpdate.js
+++ sitio/public/javascripts/profileUpdate.js
@@ -342,44 +342,6 @@ window.addEventListener('load', () => {
             $('form-profileUpdate').submit()
         }
     })
-
- window.addEventListener('load', () => {
-    let forms = document.querySelectorAll('#eliminar-usuario');
-    for (let i = 0; i < forms.length; i++) {
-      forms[i].addEventListener('submit', event => {
-        event.preventDefault();
-        Swal.fire({
-          customClass: {
-            confirmButton: 'swalBtnColor',
-            cancelButton: 'swalBtnColor'
-          },
-
-          title: '¿Estas seguro que quieres eliminar tu cuenta?',
-          text: "Esta accion es irreversible!",
-          icon: 'warning',
-          background: "#ffe4ec",
-          showCancelButton: true,
-          confirmButtonColor: '#f5deb3',
-          cancelButtonColor: '#f5deb3',
-          confirmButtonText: 'Eliminar',
-          showClass: {
-            popup: 'animate__animated animate__fadeInDown'
-          },
-
-        }).then((result) => {
-          if (result.isConfirmed) {
-
-
-
-            forms[i].submit();
-          }
-
-        })
-
-      })
-
-    }
-  })
 })
 window.addEventListener('load', () => {
     let forms = document.querySelectorAll('#eliminar-usuario');
